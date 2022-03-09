b4d7ad55cce5a9f1d867449d3cd37b14a898b21c
UO283069 <UO283069@EII-L02-13.mshome.net>
2022-03-09 19:56:07 +0100

    Commiting changes for Tromino done on class.

--- src/main/java/algstudent/s3/Tromino.java
+++ src/main/java/algstudent/s3/Tromino.java
@@ -5,6 +5,7 @@ public class Tromino {
 	private static int[][] board;
 
 	public void printBoard(int n) {
+		System.out.println();
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				System.out.print(board[i][j] + "\t");
@@ -17,72 +18,137 @@ public class Tromino {
 
 		board = new int[n][n];
 
-		board[i][j] = 0;
+		board[i][j] = 1;
+
+		int color = 2;
 
 		if (n == 2) {
-			simpleCase(i, j);
+			simpleCase(i, j, 0, 0, color);
 		} else {
-			trominoPlacing(i, j, n);
+			trominoPlacing(0, 0, n, i, j, color);
 		}
 
 	}
 
-	private void trominoPlacing(int i, int j, int n) {
+	private void trominoPlacing(int startingX, int startingY, int n, int emptyX, int emptyY, int color) {
 
-		// Place a peace depending on the position of the occupied space
-		if (i < n / 2 && j < n / 2) {
-			board[n / 2][n / 2] = 1;
-			board[(n / 2) - 1][n / 2] = 1;
-			board[n / 2][(n / 2) - 1] = 1;
-			trominoPlacing(n / 2, n / 2, n / 2);
-			trominoPlacing((n / 2) - 1, n / 2, n / 2);
-			trominoPlacing(n / 2, (n / 2) - 1, n / 2);
-		} else if (i < n / 2 && j > n / 2) {
-			board[(n / 2) - 1][(n / 2) - 1] = 1;
-			board[(n / 2)][(n / 2) - 1] = 1;
-			board[(n / 2)][(n / 2)] = 1;
-			trominoPlacing((n / 2) - 1, (n / 2) - 1, n / 2);
-			trominoPlacing(n / 2, (n / 2) - 1, n / 2);
-			trominoPlacing(n / 2, n / 2, n / 2);
-		} else if (i > n / 2 && j < n / 2) {
-			board[n / 2][n / 2] = 1;
-			board[(n / 2) - 1][n / 2] = 1;
-			board[(n / 2) - 1][(n / 2) - 1] = 1;
-			trominoPlacing(n / 2, n / 2, n / 2);
-			trominoPlacing((n / 2) - 1, n / 2, n / 2);
-			trominoPlacing((n / 2) - 1, (n / 2) - 1, n / 2);
-		} else {
-			board[(n / 2) - 1][(n / 2) - 1] = 1;
-			board[(n / 2) - 1][n / 2] = 1;
-			board[(n / 2)][(n / 2) - 1] = 1;
-			trominoPlacing((n / 2) - 1, (n / 2) - 1, n / 2);
-			trominoPlacing((n / 2) - 1, n / 2, n / 2);
-			trominoPlacing(n / 2, (n / 2) - 1, n / 2);
+		// Stop condition: return to simple case
+		if (n <= 2) {
+			simpleCase(emptyX, emptyY, startingX, startingY, color);
+			return;
 		}
 
-		trominoPlacing(i, j, n / 2);
+		// Place a piece depending on the position of the occupied space
+		if (emptyX < n / 2 && emptyY < n / 2) { // 1º Quadrant
+
+			board[startingX + n / 2][startingY + n / 2] = color;
+			board[startingX + (n / 2) - 1][startingY + n / 2] = color;
+			board[startingX + n / 2][startingY + (n / 2) - 1] = color;
+			++color;
+			trominoPlacing(startingX, startingY, n / 2, emptyX, emptyY, color); // Go to 1st q
+			++color;
+			trominoPlacing(startingX, startingY + (n / 2), n / 2, startingX + (n / 2) - 1, startingY + (n / 2) - 2,
+					color); // Go to 2nd q
+			++color;
+			trominoPlacing(startingX + (n / 2), startingY, n / 2, startingX + (n / 2) - 2, startingY + (n / 2) - 1,
+					color); // Go to 3rd
+			++color;
+			trominoPlacing(startingX + (n / 2), startingY + (n / 2), n / 2, startingX + (n / 2) - 2,
+					startingY + (n / 2) - 2, color); // Go to 4rth q
+			++color;
+
+		} else if (emptyX < n / 2 && emptyY > n / 2) { // 2º Quadrant
+
+			board[(n / 2) - 1][(n / 2) - 1] = color;
+			board[(n / 2)][(n / 2) - 1] = color;
+			board[(n / 2)][(n / 2)] = color;
+			++color;
+			trominoPlacing(startingX, startingY, n / 2, startingX + (n / 2) - 1, startingY + (n / 2) - 1, color); // Go
+																													// to
+																													// 1st
+																													// q
+			++color;
+			trominoPlacing(startingX, startingY + (n / 2), n / 2, emptyX, emptyY, color); // Go to 2nd q
+			++color;
+			trominoPlacing(startingX + (n / 2), startingY, n / 2, startingX + (n / 2) - 2, startingY + (n / 2) - 1,
+					color); // Go to 3rd
+			++color;
+			trominoPlacing(startingX + (n / 2), startingY + (n / 2), n / 2, startingX + (n / 2) - 2,
+					startingY + (n / 2) - 2, color); // Go to 4rth q
+			++color;
+
+		} else if (emptyX > n / 2 && emptyY < n / 2) { // 3º Quadrant
+			board[n / 2][n / 2] = color;
+			board[(n / 2) - 1][n / 2] = color;
+			board[(n / 2) - 1][(n / 2) - 1] = color;
+			++color;
+			trominoPlacing(startingX, startingY, n / 2, startingX + (n / 2) - 1, startingY + (n / 2) - 1, color); // Go
+																													// to
+																													// 1st
+																													// q
+			++color;
+			trominoPlacing(startingX, startingY + (n / 2), n / 2, startingX + (n / 2) - 1, startingY + (n / 2) - 2,
+					color); // Go to 2nd q
+			++color;
+			trominoPlacing(startingX + (n / 2), startingY, n / 2, emptyX, emptyY, color); // Go to 3rd
+			++color;
+			trominoPlacing(startingX + (n / 2), startingY + (n / 2), n / 2, startingX + (n / 2) - 2,
+					startingY + (n / 2) - 2, color); // Go to 4rth q
+			++color;
+		} else { // 4º Quadrant
+			board[(n / 2) - 1][(n / 2) - 1] = color;
+			board[(n / 2) - 1][n / 2] = color;
+			board[(n / 2)][(n / 2) - 1] = color;
+			++color;
+			trominoPlacing(startingX, startingY, n / 2, startingX + (n / 2) - 1, startingY + (n / 2) - 1, color); // Go
+																													// to
+																													// 1st
+																													// q
+			++color;
+			trominoPlacing(startingX, startingY + (n / 2), n / 2, startingX + (n / 2) - 1, startingY + (n / 2) - 2,
+					color); // Go to 2nd q
+			++color;
+			trominoPlacing(startingX + (n / 2), startingY, n / 2, startingX + (n / 2) - 2, startingY + (n / 2) - 1,
+					color); // Go to 3rd
+			++color;
+			trominoPlacing(startingX + (n / 2), startingY + (n / 2), n / 2, emptyX, emptyY, color); // Go to 4rth q
+		}
+		++color;
 
 	}
 
-	public void simpleCase(int i, int j) {
+	public void simpleCase(int i, int j, int startingX, int startingY, int color) {
 		// Placing the piece of the simple case
 		int n = 2;
-		if (i < n / 2 && j < n / 2) {
-			board[n / 2][n / 2] = 1;
-			board[(n / 2) - 1][n / 2] = 1;
-			board[n / 2][(n / 2) - 1] = 1;
-		} else if (i < n / 2 && j > n / 2) {
-			board[(n / 2) - 1][(n / 2) - 1] = 1;
-			board[(n / 2)][(n / 2) - 1] = 1;
-			board[(n / 2)][(n / 2)] = 1;
-		} else if (i > n / 2 && j < n / 2) {
-			board[n / 2][n / 2] = 1;
-			board[(n / 2) - 1][n / 2] = 1;
-			board[(n / 2) - 1][(n / 2) - 1] = 1;
+
+		// Position reducing to meet 2x2 size
+		if (i % 2 == 0) {
+			i = 0;
 		} else {
-			board[(n / 2) - 1][(n / 2) - 1] = 1;
-			board[(n / 2) - 1][n / 2] = 1;
-			board[(n / 2)][(n / 2) - 1] = 1;
+			i = 1;
+		}
+		if (j % 2 == 0) {
+			j = 0;
+		} else {
+			j = 1;
+		}
+
+		if (i == 0 && j == 0) {
+			board[startingX + n / 2][startingY + n / 2] = color;
+			board[startingX + (n / 2) - 1][startingY + n / 2] = color;
+			board[startingX + n / 2][startingY + (n / 2) - 1] = color;
+		} else if (i == 0 && j == 1) {
+			board[startingX + (n / 2) - 1][startingY + (n / 2) - 1] = color;
+			board[startingX + (n / 2)][startingY + (n / 2) - 1] = color;
+			board[startingX + (n / 2)][startingY + (n / 2)] = color;
+		} else if (i == 1 && j == 0) {
+			board[startingX + n / 2][startingY + n / 2] = color;
+			board[startingX + (n / 2) - 1][startingY + n / 2] = color;
+			board[startingX + (n / 2) - 1][startingY + (n / 2) - 1] = color;
+		} else {
+			board[startingX + (n / 2) - 1][startingY + (n / 2) - 1] = color;
+			board[startingX + (n / 2) - 1][startingY + n / 2] = color;
+			board[startingX + (n / 2)][startingY + (n / 2) - 1] = color;
 		}
 	}
 }
